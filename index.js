bf507d478357ee63780faffce520adf57ad63652
Ashirbad Subudhi <ashirbad5subudhi@gmail.com>
2020-07-26 20:02:34 +0530

    changed PORT number

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('./configs/mongoose');
 const app = express();
 
 //defining port
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded());
 
